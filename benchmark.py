dd5d83d1943aab2c056dbcfdec6d0a65fd8fa4d8
zhajin1997 <1151903466@qq.com>
2022-03-11 16:08:25 +0800

    update benchmark: pool path

--- benchmark.py
+++ benchmark.py
@@ -47,7 +47,7 @@ seconds = 10 # followed by 10 seconds operation (only operation will be measured
 pibench_path = "pibench/build/src/PiBench" # default path to pibench executable
 lib_dir = "wrappers" # default path to binary folder
 result_dir = "./results" # default path to folder for which results will be saved into
-pool_path = "/mnt/pmem" # by default PMem pool will be created in current dir
+pool_path = "/mnt/pmem/" # by default PMem pool will be created in current dir
 
 numactl = "numactl --membind=0"
 base_command = "sudo LD_PRELOAD=/home/aim/hjn/tree/jemalloc-5.2.1/lib/libjemalloc.so"
